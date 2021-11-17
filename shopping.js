27ebcf7c4bc3eeac3b9796815173f5dc90e0ede1
jbyrd4 <jbyrd4@gmail.com>
2021-11-17 13:20:36 -0600

    first shopping list commit

---
+++ shopping.js
@@ -0,0 +1,60 @@
+const list = [
+    {
+        id: 1,
+        name: 'milk',
+        price: 5
+
+    },
+    
+    {
+        id: 2,
+        name: 'eggs',
+        price: 4
+
+    },
+
+    {
+        id: 3,
+        name: 'pizza rolls',
+        price: 8
+
+    },
+
+    {
+        id: 4,
+        name: 'ground beef',
+        price: 5
+
+    },
+
+    {
+        id: 5,
+        name: 'banana',
+        price: '.25'
+
+    },
+
+    {
+        id: 6,
+        name: 'hot dog buns',
+        price: 3
+
+    },
+
+    {
+        id: 7,
+        name: 'ketchup',
+        price: 3
+
+    },
+
+    {
+        id: 8,
+        name: 'hummus',
+        price: 4
+
+    }
+    
+];
+
+console.log(list);
